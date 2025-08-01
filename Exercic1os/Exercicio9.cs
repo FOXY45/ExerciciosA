8cde5dbaa6564cc31b842a93531fa71c5154fc4b
FOXY45 <otakufididobr123@gmail.com>
2025-08-01 17:15:38 -0300

    Add exercicio9

---
+++ Exercic1os/Exercicio9.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Globalization;
+//OPERADORES LOGICOS
+namespace Exercic1os
+{
+    public static class Exercicio9
+    {
+        public static void Executar()
+        {
+            Console.WriteLine("oi");
+        }
+    }
+}
